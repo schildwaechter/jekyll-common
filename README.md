922ad7461b56ee0b71506419ba1bdd36560ef45d
Radovan Bast <bast@users.noreply.github.com>
2019-11-17 17:06:29 +0100

    copy JS and CSS from https://github.com/carpentries/styles
    to be more similar to https://carpentries.github.io/lesson-example/

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 These sources are to be included in lesson repositories as Git submodules
 to simplify updates and avoid accidental code divergence.
+
+The code under `assets/` and `includes/` has been copied from
+https://github.com/carpentries/styles which is copyright
+[Software Carpentry](https://software-carpentry.org)
+and provided under a [CC-BY-4.0 license](https://github.com/carpentries/styles/blob/gh-pages/LICENSE.md).
--- assets/js/lesson.js
+++ assets/js/lesson.js
@@ -1,32 +1,9 @@
-/*
- * Code from https://github.com/swcarpentry/workshop-template/ distributed under MIT License.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- * */
-
 // Make all tables striped by default.
 $("table").addClass("table table-striped");
 
 
 // Handle foldable challenges and solutions (on click and at start).
-$(".challenge,.discussion,.solution").click(function(event) {
+$(".solution").click(function(event) {
     var trigger = $(event.target).has(".fold-unfold").size() > 0
                || $(event.target).filter(".fold-unfold").size() > 0;
     if (trigger) {
@@ -35,7 +12,7 @@ $(".challenge,.discussion,.solution").click(function(event) {
         event.stopPropagation();
     }
 });
-$(".challenge,.discussion,.solution").each(function() {
+$(".solution").each(function() {
     $(">*:not(h2)", this).toggle();
     var h2 = $("h2:first", this);
     h2.append("<span class='fold-unfold glyphicon glyphicon-collapse-down'></span>");
@@ -49,3 +26,31 @@ function google_search() {
   var domain = $("meta[name=search-domain]").attr("value");
   window.open("https://www.google.com/search?q=" + query + "+site:" + domain);
 }
+
+// function to shrink the life cycle bar when scrolling
+$(function(){
+    $('#life-cycle').data('size','big');
+});
+
+$(window).scroll(function(){
+    if($(document).scrollTop() > 0)
+    {
+        if($('#life-cycle').data('size') == 'big')
+        {
+            $('#life-cycle').data('size','small');
+            $('#life-cycle').stop().animate({
+                padding: '5px'
+            },100);
+        }
+    }
+    else
+    {
+        if($('#life-cycle').data('size') == 'small')
+        {
+            $('#life-cycle').data('size','big');
+            $('#life-cycle').stop().animate({
+                padding: '15px'
+            },100);
+        }
+    }
+});
